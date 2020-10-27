c8b1a500061b79e928cd65ef6b8b7cdcbefb8322
Pvlvlkv <59417591+Pvlvlkv@users.noreply.github.com>
2020-10-27 15:26:12 +0200

    arows and close

--- task.js
+++ task.js
@@ -26,6 +26,7 @@ gallery.innerHTML = galleryItems;
 
 const modal = document.querySelector(".js-lightbox");
 const modalInnerImage = document.querySelector(".lightbox__image");
+const modalInnerContent = document.querySelector(".lightbox__content");
 gallery.addEventListener("click", onGalleryClick);
 
 /** gallery  */
@@ -33,29 +34,77 @@ function onGalleryClick(e) {
   e.preventDefault();
 
   modal.classList.add("is-open");
-  console.log();
-  const currentUrl = e.target.dataset.source;
-  modalInnerImage.src = currentUrl;
+  modalInnerImage.src = e.target.dataset.source;
+
   const modalClose = document.querySelector(".is-open");
   modalClose.addEventListener("click", onModalClose);
-  modalClose;
-  addEventListener("keyup", onModalClose);
-}
 
-/**close button */
-const modalCloseBtn = document.querySelector(".lightbox__button");
+  const modalCloseBtn = document.querySelector(".lightbox__button");
+  modalCloseBtn.addEventListener("click", onModalCloseBtnClick);
 
-modalCloseBtn.addEventListener("click", onModalCloseBtnClick);
-function onModalCloseBtnClick(e) {
-  modal.classList.remove("is-open");
-  modalInnerImage.src = "";
-}
-/**overlay close*/
-function onModalClose(e) {
-  if (e.target === document.querySelector(".lightbox__image")) {
-    return;
+  modalCloseBtn.addEventListener("click", onModalCloseBtnClick);
+  function onModalCloseBtnClick(e) {
+    modal.classList.remove("is-open");
+    modalInnerImage.src = "";
   }
-  modal.classList.remove("is-open");
-  modalInnerImage.src = "";
+  /**overlay close*/
+  function onModalClose(e) {
+    if (e.target === document.querySelector(".lightbox__image")) {
+      return;
+    }
+    modal.classList.remove("is-open");
+    modalInnerImage.src = "";
+  }
+  /**esc button close */
+  const galleryEl = document.querySelector(".js-gallery");
+
+  galleryEl.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      modal.classList.remove("is-open");
+      modalInnerImage.src = "";
+    }
+  });
 }
-/**esc button close */
+
+/** change to next or prev by arrows */
+let currentModalUrl = "";
+let nexturl;
+gallery.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowRight") {
+    if (currentModalUrl === "") {
+      currentModalUrl = modalInnerImage.src;
+    }
+    if (currentModalUrl !== "") {
+      items.forEach((el, index) => {
+        if (index === 8) {
+          return;
+        }
+        if (el.original === currentModalUrl) {
+          nexturl = items[`${index + 1}`].original;
+        }
+      });
+
+      currentModalUrl = nexturl;
+    }
+  }
+  modalInnerImage.src = nexturl;
+});
+gallery.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowLeft") {
+    if (currentModalUrl === "") {
+      currentModalUrl = modalInnerImage.src;
+    }
+    if (currentModalUrl !== "") {
+      items.find((el, index) => {
+        if (index === 0) {
+          return;
+        }
+        if (el.original === currentModalUrl) {
+          nexturl = items[`${index - 1}`].original;
+        }
+      });
+      currentModalUrl = nexturl;
+    }
+  }
+  modalInnerImage.src = nexturl;
+});
